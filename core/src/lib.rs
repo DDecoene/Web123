48839dc628ead61a12d91b2f431e1a36126cdd63
Dennis Decoene <DDecoene@users.noreply.github.com>
2026-09-13 22:46:34 +0200

    Wire the wasm-bindgen wrapper to the new recalc engine

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -4,3 +4,34 @@ mod model;
 mod parser;
 mod functions;
 mod engine;
+
+use wasm_bindgen::prelude::*;
+
+use engine::SpreadsheetCore;
+use model::CellAddr;
+
+#[wasm_bindgen]
+pub struct Spreadsheet {
+    core: SpreadsheetCore,
+}
+
+#[wasm_bindgen]
+impl Spreadsheet {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Spreadsheet {
+        Spreadsheet { core: SpreadsheetCore::new() }
+    }
+
+    #[wasm_bindgen(js_name = setCell)]
+    pub fn set_cell(&mut self, reference: &str, input: &str) {
+        self.core.set_cell(reference, input);
+    }
+
+    #[wasm_bindgen(js_name = getDisplay)]
+    pub fn get_display(&self, reference: &str) -> String {
+        match CellAddr::parse(reference) {
+            Some(addr) => self.core.display(addr),
+            None => String::new(),
+        }
+    }
+}
